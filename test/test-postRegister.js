7e9e9bf5d9627ba7cc101ed118716112fa46f052
seerapusairam <seerapu.sai123@gmail.com>
2025-08-11 17:49:06 -0400

    Added test for postRegister

---
+++ test/test-postRegister.js
@@ -0,0 +1,43 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const expect = chai.expect;
+
+const { postRegister } = require('../controller/userController');
+const { badRequestError, unauthenticatedError } = require('../Error/allErrors');
+
+const model = require('../Model/userSchema');
+
+describe('postRegister', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+it('should return user and JWT Token successsfully', async () => {
+    const req = { body: { name:"Test user", email: 'test@gmail.com', password: 'correctpassword' } };
+
+    const fakeUser = {
+        name: 'Test user',
+        createJWT: sinon.stub().resolves('jwt-token') // this one is returning jwt token
+    }
+
+    sinon.stub(model, 'create').resolves(fakeUser)
+
+    const res = {
+        status: sinon.stub().returnsThis(), // to mock the status code res.status(200)
+        json: sinon.stub() // to mock the json body .json({message:"mock"})
+    };
+
+    await postRegister(req, res);
+
+    expect(model.create.calledOnceWith({...req.body})).to.be.true;
+    expect(fakeUser.createJWT.calledOnce).to.be.true;
+    expect(res.status.calledOnceWith(201)).to.be.true;
+    expect(res.json.calledOnceWith({
+        user:{
+            user: fakeUser.name
+        },
+        token:"jwt-token"
+    })).to.be.true
+  });
+
+})
